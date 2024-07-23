ffa502fa6d2cf850f74c6f75b22094dedb4dfc5a
gespel <heimbrodt@uni-potsdam.de>
2024-07-23 23:44:46 +0200

    further work

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,52 @@
+#![allow(dead_code)]
+#![allow(unused_imports)]
+
 use std::fs::{File};
-use std::io;
+use std::{env, io};
 use std::io::prelude::*;
 use std::io::LineWriter;
+use std::path::{Path, PathBuf};
+use crate::Language::{PYTHON, RUST, UNKOWN};
+
+#[derive(Debug)]
+enum Language {
+    RUST,
+    PYTHON,
+    CLANG,
+    CPPLANG,
+    UNKOWN
+}
+
+struct HavenScanner {
+
+}
+impl HavenScanner {
+    fn new() -> HavenScanner {
+        HavenScanner {
+
+        }
+    }
+
+    fn scan(&self) -> Language {
+        //let path = env::current_dir().expect("Cannot determine current directory");
+        if self.file_exists("Cargo.toml") {
+            return RUST
+        }
+        else if self.file_exists("requirements.txt") {
+            return PYTHON
+        }
+        return UNKOWN
+    }
+
+    fn file_exists(&self, file_name: &str) -> bool {
+        return if Path::new(file_name).exists() {
+            true
+        } else {
+            false
+        }
+    }
+}
+
 
 struct HavenWriter {
     project_name: String,
@@ -27,24 +72,38 @@ impl HavenWriter {
 }
 
 fn main() -> std::io::Result<()> {
+    let hs = HavenScanner::new();
     let mut projectname = String::new();
-    let mut language = String::new();
+    let lang = hs.scan();
+    println!("Scanned current directory and found {:?} as primary language.", lang);
 
     println!("Enter the project name: ");
     io::stdin().read_line(&mut projectname)?;
-    let mut hw = HavenWriter::new(projectname.as_str());
-    println!("Enter the project language: ");
-    io::stdin().read_line(&mut language)?;
+    let mut hw = HavenWriter::new(projectname.as_str().strip_suffix("\n").unwrap());
 
-    match language.as_str().strip_suffix("\n").expect("Error while parsing input buffer!") {
+    match lang {
+        Language::RUST => {
+            hw.create_dockerfile_rust().expect("");
+        }
+        Language::PYTHON => {}
+        Language::CLANG => {}
+        Language::CPPLANG => {}
+        Language::UNKOWN => {}
+    }
+
+
+
+    /*match language.as_str().strip_suffix("\n").expect("Error while parsing input buffer!") {
         "rust" => {
             hw.create_dockerfile_rust().expect("Error while creating Dockerfile");
             println!("Dockerfile created!\n");
         }
         _ => {}
-    }
+    }*/
 
     //hw.create_dockerfile_rust().expect("Error while writing Dockerfile!");
+
+
 
     Ok(())
 }
